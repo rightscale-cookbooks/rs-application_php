39b5a25d4e1fb8291cc7541e47fa944f7001f0cd
Nitin Mohan <nitin@rightscale.com>
2014-03-14 14:52:02 -0700

    Add recipes to attributes in metadata

--- metadata.rb
+++ metadata.rb
@@ -35,7 +35,11 @@ attribute 'rs-application_php/listen_port',
   :description => 'The port to use for the application to bind. Example: 8080',
   :default => '8080',
   :required => 'optional',
-  :recipes => ['rs-application_php::default']
+  :recipes => [
+    'rs-application_php::default',
+    'rs-application_php::tags',
+    'rs-application_php::application_backend'
+  ]
 
 attribute 'rs-application_php/scm/repository',
   :display_name => 'Application Repository URL',
@@ -59,14 +63,22 @@ attribute 'rs-application_php/application_name',
   :display_name => 'Application Name',
   :description => 'The name of the application. Example: hello_world',
   :required => 'required',
-  :recipes => ['rs-application_php::default']
+  :recipes => [
+    'rs-application_php::default',
+    'rs-application_php::tags',
+    'rs-application_php::application_backend',
+    'rs-application_php::application_backend_detached'
+  ]
 
 attribute 'rs-application_php/app_root',
   :display_name => 'Application Root',
   :description => 'The path of application root relative to /usr/local/www/sites/<application name> directory. Example: my_app',
   :default => '/',
   :required => 'optional',
-  :recipes => ['rs-application_php::default']
+  :recipes => [
+    'rs-application_php::default',
+    'rs-application_php::tags'
+  ]
 
 attribute 'rs-application_php/migration_command',
   :display_name => 'Application Migration Command',
