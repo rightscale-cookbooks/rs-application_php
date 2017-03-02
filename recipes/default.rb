61a0c2cd8582b7409c827ec8db1e7e090be7b61c
Richard Shade <rshade@rightscale.com>
2017-03-02 16:52:42 -0600

    populating the node['php'] hash if packages exist

--- recipes/default.rb
+++ recipes/default.rb
@@ -35,6 +35,11 @@ mysql2_chef_gem 'default' do
   action :install
 end
 
+unless node['rs-application_php']['application_name'].nil?
+  node.override['php']['packages'] = node['rs-application_php']['application_name'].select { |s| s.match(/php/) }
+  node.override['php']['mysql']['package'] = node['rs-application_php']['application_name'].select { |s| s.match(/mysql/) }
+end
+
 include_recipe 'php::module_mysql'
 
 # Validate application name
