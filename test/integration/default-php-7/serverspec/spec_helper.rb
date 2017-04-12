c9a206c7e6e43387ff6f85ae37c17f25e4e59d90
Richard Shade <rshade@rightscale.com>
2017-04-12 17:01:59 -0500

    fixing windows char issues

--- test/integration/default-php-7/serverspec/spec_helper.rb
+++ test/integration/default-php-7/serverspec/spec_helper.rb
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-# frozen_string_literal: true
-require 'serverspec'
-require 'pathname'
-require 'json'
-
-RSpec.configure do |conf|
-  conf.path = '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
-end
-=======
 # frozen_string_literal: true
 require 'serverspec'
 require 'pathname'
@@ -18,4 +8,3 @@ set :backend, :exec
 RSpec.configure do |conf|
   conf.path = '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
 end
->>>>>>> d828cf82a88f9abcf57fbad0b3b80a16e014f6d1
